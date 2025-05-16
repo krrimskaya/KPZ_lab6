36a7e3b816b53fd56625d41ce7ad7da77a58af0a
Anastasia Vorobey <ipz231_vav@student.ztu.edu.ua>
2025-05-16 22:42:01 +0300

    Add ability to create notes in the application

--- NotesApp/Controllers/HomeController.cs
+++ NotesApp/Controllers/HomeController.cs
@@ -1,31 +1,63 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NotesApp.Data;
 using NotesApp.Models;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
-namespace NotesApp.Controllers;
-
-public class HomeController : Controller
+namespace NotesApp.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        // GET: /Home/Index
+        public async Task<IActionResult> Index()
+        {
+            var notes = await _context.Notes.ToListAsync();
+            return View(notes);
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        // GET: /Home/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Home/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Note note)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Notes.Add(note);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(note);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel 
+            { 
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier 
+            });
+        }
     }
 }
