46cb4f81b7bd4a20d6d5e8f4071a17da120b5354
Anastasia Vorobey <ipz231_vav@student.ztu.edu.ua>
2025-05-18 20:09:18 +0300

    Add password change feature

---
+++ NotesApp/Models/ForgotPasswordViewModel.cs
@@ -0,0 +1,12 @@
+// Додати новий файл Models/ForgotPasswordViewModel.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace NotesApp.Models
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required(ErrorMessage = "Email обов'язковий")]
+        [EmailAddress(ErrorMessage = "Неправильний формат email")]
+        public string Email { get; set; }
+    }
+}
---
+++ NotesApp/Models/ResetPasswordViewModel.cs
@@ -0,0 +1,24 @@
+// Додати новий файл Models/ResetPasswordViewModel.cs
+using System.ComponentModel.DataAnnotations;
+
+namespace NotesApp.Models
+{
+    public class ResetPasswordViewModel
+    {
+        [Required(ErrorMessage = "Email обов'язковий")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Пароль обов'язковий")]
+        [StringLength(100, ErrorMessage = "Пароль повинен містити мінімум {2} і максимум {1} символів.", MinimumLength = 8)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Підтвердження пароля")]
+        [Compare("Password", ErrorMessage = "Паролі не співпадають")]
+        public string ConfirmPassword { get; set; }
+
+        public string Code { get; set; }
+    }
+}
